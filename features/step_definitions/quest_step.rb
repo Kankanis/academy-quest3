03162b62659ae9fc4a0268fde4965eb414bcdeab
Kankanis <kankanis.tty@gmail.com>
2025-06-18 09:06:32 +0700

    features e2e test

--- features/step_definitions/quest_step.rb
+++ features/step_definitions/quest_step.rb
@@ -1,90 +1,93 @@
+Before do
+  Quest.destroy_all
+end
+
+# === Given Steps ===
+
 Given('there are no quests') do
   Quest.destroy_all
 end
 
-Given('there is a quest named {string}') do |name|
-  Quest.create!(name: name)
-end
-
-When('I visit the quests index page') do
+Given('I am on the quests index page') do
   visit quests_path
 end
 
-Then('I should see {string}') do |text|
-  expect(page).to have_content(text)
+
+Given('there is a quest named {string}') do |quest_name|
+  FactoryBot.create(:quest, name: quest_name)
 end
 
-Then('I should see {string} as the page title') do |title|
-  expect(page).to have_css('h1', text: title)
-end
+# === When Steps ===
 
-Then('I should not see {string}') do |text|
-  expect(page).not_to have_content(text)
-end
-
-Then('I should see a link to create a new quest') do
-  expect(page).to have_link('New quest')
-end
-
-When('I click on {string}') do |link_text|
-  click_on(link_text)
-end
-
-When('I click on {string} for {string}') do |link_text, quest_name|
-  within("##{dom_id(Quest.find_by(name: quest_name))}").find(:xpath, '..') do
-    click_on(link_text)
+When('I click {string}') do |text_or_locator|
+  begin
+    click_link text_or_locator
+  rescue Capybara::ElementNotFound
+    click_button text_or_locator
   end
 end
 
-Then('I should see the new quest form') do
-  expect(page).to have_selector('form')
-  expect(page).to have_field('Name')
-  expect(page).to have_button('Create Quest')
+When('I click the {string} button') do |button_name_or_testid_description|
+  case button_name_or_testid_description
+  when "back to quests"
+    save_and_open_page
+    element = find('[data-testid="back-to-quests-button"]', visible: :all, wait: 5)
+
+    element.click
+
+  when "plus"
+    find('[data-testid="add-quest-button"]').click
+  when "Save"
+    click_button 'Save'
+  else
+    begin
+      find_button(button_name_or_testid_description).click
+    rescue Capybara::ElementNotFound
+      find("[data-testid='#{button_name_or_testid_description}-button']").click
+    end
+  end
 end
 
-When('I fill in {string} with {string}') do |field, value|
-  fill_in(field, with: value)
+# === Then Steps ===
+
+Then('I should see {string} as the page title') do |title_text|
+  expect(page).to have_title title_text
 end
 
-# Helper method to get the DOM ID for a record
-def dom_id(record)
-  ActionView::RecordIdentifier.dom_id(record)
+Then('I should see {string}') do |text|
+  expect(page).to have_content(text, wait: 5)
 end
 
-# Additional step definitions for quest status
-
-When('I check {string}') do |checkbox_label|
-  check(checkbox_label)
+Then('I should not see {string}') do |text|
+  expect(page).to have_no_content(text, wait: 5)
 end
 
-When('I uncheck {string}') do |checkbox_label|
-  uncheck(checkbox_label)
-end
 
-Then('I should see the quest is {string}') do |status|
-  expect_status = status == "completed" ? "Yes" : "No"
-  expect(page).to have_content("Status: #{expect_status}")
-end
-
-# Steps for verifying flash messages
-Then('I should see a success message {string}') do |message|
-  expect(page).to have_css(".bg-green-50", text: message)
-end
-
-# Steps for testing the brag functionality
-When('I click on the brag button') do
-  click_on("Brag")
-end
 
 Then('I should be on the brag page') do
-  expect(page).to have_current_path(brag_path)
+  expect(current_path).to eq(brag_path)
 end
 
-# Steps for handling confirmation dialogs
-When('I confirm the dialog') do
-  page.driver.browser.switch_to.alert.accept
+Then('I should be on the quests index page') do
+  expect(page).to have_current_path("/", wait: 10)
 end
 
-When('I dismiss the dialog') do
-  page.driver.browser.switch_to.alert.dismiss
+
+
+Then('I should see an input field with placeholder {string}') do |placeholder_text|
+  expect(page).to have_field(placeholder_text)
+end
+
+Then('I should see the {string} button') do |button_name_or_testid_description|
+  case button_name_or_testid_description
+  when "plus"
+    expect(page).to have_selector('button i.fas.fa-plus[data-testid="add-quest-button"]')
+  when "back to quests"
+    expect(page).to have_selector('[data-testid="back-to-quests-button"]')
+  when "Save"
+    expect(page).to have_button("Save")
+  else
+
+    expect(page).to have_button(button_name_or_testid_description)
+  end
 end
